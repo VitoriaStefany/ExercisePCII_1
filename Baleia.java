6fde141244aeb2d5312b33311aab15303343c0c7
Vitória Stefany <108250663+VitoriaStefany@users.noreply.github.com>
2023-09-11 21:04:43 -0300

    Add files via upload

---
+++ Baleia.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercícioaula02;
+
+/**
+ *
+ * @author Vitória Stefany
+ */
+public class Baleia extends Mamífero {
+    private String especie;
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+}
---
+++ Cao.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercícioaula02;
+
+/**
+ *
+ * @author Vitória Stefany
+ */
+public class Cao extends Mamífero {
+    
+    private String raça;
+    private String porte;
+    
+    public String getRaça() {
+        return raça;
+    }
+
+    public void setRaça(String raça) {
+        this.raça = raça;
+    }
+
+    public String getPorte() {
+        return porte;
+    }
+
+    public void setPorte(String porte) {
+        this.porte = porte;
+    }
+    
+    
+}
